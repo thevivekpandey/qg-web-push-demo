7ca7bb39e1666a557b18e0e274019876dd45b5a3
Vivek.Pandey <vivek.pandey@VivekPandeys-MacBook-Pro.local>
2018-07-15 00:05:45 +0800

    ryan's demo

---
+++ sdk-loader.js
@@ -0,0 +1,13 @@
+
+    var AIQUA_APP_ID = 'ad55582957817e511c3d';
+
+    !function(q,g,r,a,p,h,js){
+      if(q.qg)return;
+      js = q.qg = function() {
+        js.callmethod ? js.callmethod.call(js, arguments) : js.queue.push(arguments);
+      };
+      js.queue = [];
+      p=g.createElement(r);p.async=!0;p.src=a;h=g.getElementsByTagName(r)[0];
+      h.parentNode.insertBefore(p,h);
+    }(window,document,'script','//cdn.qgr.ph/qgraph.ad55582957817e511c3d.js');
+  
